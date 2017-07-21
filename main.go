5c6b6df898c88fc659f108d21d4874936d1fd655
Janos Matyas <janos.matyas@gmail.com>
2017-07-21 11:21:37 +0100

    use melody for websockets. chi for http vs gin?

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
-	"github.com/matyix/echo/conf"
-	"github.com/gin-gonic/gin"
-	"github.com/olahol/melody"
 	"github.com/gin-gonic/contrib/ginrus"
-	"time"
-	"github.com/matyix/echo/middlewares"
-	"crypto/tls"
-	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/matyix/echo/conf"
 	"github.com/matyix/echo/log"
+	"github.com/matyix/echo/middlewares"
+	"github.com/olahol/melody"
+	"net/http"
+	"time"
 )
 
 var GitCommit string
@@ -40,10 +40,86 @@ func main() {
 	log.WithFields(log.Fields{
 		"EventName": "set_gin_mode",
 		"Mode":      cfg.GetString("mode"),
-	}).Debug("Setting gin mode to ", cfg.GetString("mode"))
+	}).Debug("Setting gin_ mode to ", cfg.GetString("mode"))
 	gin.SetMode(cfg.GetString("mode"))
 
-	//r := gin.New()
-	//m := melody.New()
+	ginweb := gin.New()
+	melodyws:= melody.New()
 
+	//Web Sockets
+	authorized.GET("/channel/:name/websockets", func(c *gin.Context) {
+		melodyws.HandleRequest(c.Writer, c.Request)
+	})
+
+	ginweb.Use(ginrus.Ginrus(log.NewLogger(cfg), time.RFC3339, true))
+	ginweb.Use(gin.Recovery())
+
+	melodyws.HandleConnect(func(session *melody.Session) {
+		log.WithFields(log.Fields{
+			"EventName":     "websockets_client_connect",
+			"RemoteAddress": session.Request.RemoteAddr,
+		}).Debug("new websockets client connected ", session.Request.RemoteAddr)
+	})
+
+	melodyws.HandleDisconnect(func(session *melody.Session) {
+		log.WithFields(log.Fields{
+			"EventName":     "websockets_client_disconnect",
+			"RemoteAddress": session.Request.RemoteAddr,
+		}).Debug("WebSockets client disconnected ", session.Request.RemoteAddr)
+	})
+
+	melodyws.HandleError(func(session *melody.Session, err error) {
+		log.WithFields(log.Fields{
+			"EventName":     "websockets_error",
+			"RemoteAddress": session.Request.RemoteAddr,
+			"Error":         err.Error(),
+		}).Error("error ocurred with WebSockets client ", err.Error())
+	})
+
+	melodyws.HandleMessage(func(session *melody.Session, msg []byte) {
+		melodyws.BroadcastFilter(msg, func(q *melody.Session) bool {
+			msgStr := string(msg[:])
+
+			log.WithFields(log.Fields{
+				"EventName": "websockets_client_message",
+				"Message":   msgStr,
+				"Channel":   q.Request.URL.Path,
+			}).Debug("reseived message: ", msgStr)
+
+			return q.Request.URL.Path == session.Request.URL.Path
+		})
+	})
+
+	// Start Server
+	fmt.Printf("STARTED", "started" )
+	log.WithFields(log.Fields{
+		"EventName":         "start",
+		"ListenAddress":     cfg.GetString("listen_address"),
+		"GitCommit":         GitCommit,
+		"Version":           Version,
+		"VersionPrerelease": VersionPrerelease,
+		//"TLS": cfg.GetBool("secure"),
+		"ReadTimeout": cfg.GetDuration("read_timeout"),
+		"WriteTimeout": cfg.GetDuration("write_timeout"),
+		"MaxHeaderBytes": cfg.GetInt("max_header_bytes"),
+	}).Info("starting server and listening on ", cfg.GetString("listen_address"))
+
+	server := &http.Server{
+		Addr:           cfg.GetString("listen_address"),
+		Handler:        ginweb,
+		ReadTimeout:    cfg.GetDuration("read_timeout"),
+		WriteTimeout:   cfg.GetDuration("write_timeout"),
+		MaxHeaderBytes: cfg.GetInt("max_header_bytes"),
+		//TLSConfig:      tlscfg,
+
+	}
+
+	switch cfg.GetBool("secure") {
+	case true:
+		// Key or DH parameter strength >= 4096 bits (e.g., 4096)
+		log.Fatal(server.ListenAndServeTLS(cfg.GetString("cert_file"), cfg.GetString("key_file")))
+	default:
+		log.Fatal(server.ListenAndServe())
+	}
 }
+
